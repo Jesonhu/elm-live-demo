dd608fe9a69fa2ef3be2eb158fda0138fa8cd4b6
jesonhu <jesonhu_web@163.com>
2017-04-18 14:35:26 +0800

    购物车

--- src/mycart/js/address.js
+++ src/mycart/js/address.js
@@ -1,41 +1,44 @@
-var vmAddress = new Vue({
-	el:'.container',
-	data:{
-		addressList:new Array(),
-		shippingMethod:1,
-		limitNum:3,
-		addressIndex:0,
-		currentDefaultAddress:{},
+let vmAddress = new Vue({
+	el: '.container',
+	data: {
+		limitNum: 3, //限制地址列表显示的长度
+		addressList: [],
+		currentIndex: 0, //用来保存当前点击的li
+		shippingMethod: 1, //配送类型1 标准 2高级
 	},
-	mounted:function(){
-		this.$nextTick(function(){
+	mounted: function () {
+		this.$nextTick(function () {
+			this.addressView();
+		})
+	},
+	methods: {
+		addressView: function () {
 			axios.get('data/address.json')
-			.then(retObj=>{
-				if(retObj.status == 200){
-					this.addressList = retObj.data.result;
-					this.addressList.forEach(address=>{
-						if(address.isDefault){
-							this.currentDefaultAddress = address;
-						}
-					});
+			.then((res) => {
+				if(res.status === 200){
+					this.addressList = res.data.result;
 				}
 			})
-		});
-	},
-	methods:{
-		setDefaultAddress:function(address){
-			this.currentDefaultAddress = address;
-			address.isDefault = true;
+			.catch((err) => {
+				console.log('get address data error');
+			})
 		},
+		loadMore: function () {
+			this.limitNum = (this.limitNum==3)?this.addressList.length:3; //显示3条或显示全部
+		},
+		setDefault: function (_index) {
+			this.addressList.forEach((address, index) => {
+				address.isDefault = false;
+				_index == index && (address.isDefault = true); //点击当前的li设为默认地址
+			});
+		},
+		delAddress: function (_index) {
+			this.addressList.splice(_index,1);
+		}
 	},
-	computed:{
-		filteAddress:function(){
+	computed: {
+		filterAddress: function () {
 			return this.addressList.slice(0,this.limitNum);
-		},
-	},
-	watch:{
-		currentDefaultAddress:function(newVal,oldVal){
-			oldVal.isDefault = false;
 		},
 	}
 });
